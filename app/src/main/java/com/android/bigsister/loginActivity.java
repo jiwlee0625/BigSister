aceb81ce81452eeda83e20dc5f28c5ffc44eda9f
jiwlee0625 <jiwlee.gatech@gmail.com>
2018-02-11 01:28:03 -0500

    login and signup
    meh

--- app/src/main/java/com/android/bigsister/loginActivity.java
+++ app/src/main/java/com/android/bigsister/loginActivity.java
@@ -7,7 +7,10 @@ public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("On create of loginactivity");
         super.onCreate(savedInstanceState);
+        System.out.println("After super create");
         setContentView(R.layout.activity_login);
+        System.out.println("After layout");
     }
 }
--- app/src/main/java/com/android/bigsister/notLoggedInActivity.java
+++ app/src/main/java/com/android/bigsister/notLoggedInActivity.java
@@ -1,7 +1,10 @@
 package com.android.bigsister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class notLoggedInActivity extends AppCompatActivity {
 
@@ -9,6 +12,25 @@ public class notLoggedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_not_logged_in);
+
+        Button loginButton = (Button) findViewById(R.id.Login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Button clicked!");
+                Intent loginActivityIntent = new Intent(getApplicationContext(), loginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
+
+        Button signUpButton = (Button) findViewById(R.id.signUp);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpActivityIntent = new Intent(getApplicationContext(), signUpActivity.class);
+                startActivity(signUpActivityIntent);
+            }
+        });
     }
 
 
