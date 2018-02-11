55a1d0a0c15ad6f9129a34e0e664b68754efcf51
ykim0113 <ykim0113@gmail.com>
2018-02-10 22:38:29 -0500

    main page update

--- app/src/main/java/com/android/bigsister/MainActivity.java
+++ app/src/main/java/com/android/bigsister/MainActivity.java
@@ -1,7 +1,11 @@
 package com.android.bigsister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
