2f9e30e80bc4e6adeaf5c4f4c777369e96c43ed0
jiwlee0625 <jiwlee.gatech@gmail.com>
2018-02-11 09:42:58 -0500

    meh

--- app/src/main/java/com/android/bigsister/threatActivity.java
+++ app/src/main/java/com/android/bigsister/threatActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.view.MotionEvent;
+import com.google.firebase.database.*;
 
 public class threatActivity extends AppCompatActivity {
     private boolean underThreat = false;
@@ -12,8 +15,7 @@ public class threatActivity extends AppCompatActivity {
     private String userFirstName;
     private String userLastName;
     private boolean alertedYet = false;
-    private long lastDown = 0;
-    private long lastDuration = 0;
+    final Timer timer = new Timer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,28 +25,15 @@ public class threatActivity extends AppCompatActivity {
         threatButton.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(underThreat && event.getAction() == MotionEvent.ACTION_DOWN) {
-                    lastDown = System.currentTimeMillis();
-                } else if (underThreat && event.getAction() == MotionEvent.ACTION_UP) {
-                    lastDuration = System.currentTimeMillis() - lastDown;
-                    if (underThreat && alertedYet == false && lastDuration >= 10000) {
-                        //alrt system happen
-                        //sendAlert();
-                        System.out.println("Alert system called: " + lastDown + " my time: " + System.currentTimeMillis());
-                        alertedYet = true;
-                    }
-                }
-                return true;
+
             }
         });
         //Ji won lee needs help! Location is ______________________________
-        Button deactivateButton = (Button) findViewById(R.id.stopButton);
+        Button deactivateButton = (Button) findViewById(R.id.startStopButton);
         deactivateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                underThreat = false;
-                lastDown = 0;
-                lastDuration = 0;
+                System.out.println("startStopButton Button Clicked.");
             }
         });
     }
