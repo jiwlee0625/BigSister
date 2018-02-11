ff688851898a9aba4ca4334913177a88595e19a1
jiwlee0625 <jiwlee.gatech@gmail.com>
2018-02-11 04:03:15 -0500

    login fucking works hell yes

--- app/src/main/java/com/android/bigsister/loginActivity.java
+++ app/src/main/java/com/android/bigsister/loginActivity.java
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.support.annotation.NonNull;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
 
 public class loginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -17,15 +23,15 @@ public class loginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
         Button loginButton = (Button) findViewById(R.id.loginButton);
-        EditText emailInput = (EditText)findViewById(R.id.emailInput);
-        EditText pwInput = (EditText)findViewById(R.id.pwInput);
+        final EditText emailInput = (EditText)findViewById(R.id.emailInput);
+        final EditText pwInput = (EditText)findViewById(R.id.pwInput);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailInput.getText().toString();
                 String password = pwInput.getText().toString();
                 mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(loginActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
